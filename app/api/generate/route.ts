765049b65057e9ec0fb67d84330040f97b94f507
vineet agarwal <vineetagarwal@vineets-MacBook-Air.local>
2025-03-18 01:35:27 +0530

    fix: rate limit fixed

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -25,7 +25,7 @@ const redis = new Redis({
 // Create a new ratelimiter that allows 10 requests per 10 seconds
 const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(1, "30 s"),
+  limiter: Ratelimit.slidingWindow(2, "30 s"),
   analytics: true,
 });
 
